5e62ad0be27f920671e330c93ed1bea8dbd13f95
pengwan <pwan@aerohive.com>
2016-04-08 17:45:12 +0800

    modify

--- demo1/bundle.js
+++ demo1/bundle.js
@@ -58,6 +58,31 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+	var action = {
+	    type: 'add'
+	};
+
+	function reducer(state, action) {
+	    switch (action.type) {
+	        case 'add':
+	            return state + 1;
+	        case 'reduce':
+	            return state - 1;
+	        default:
+	            return state;
+	    }
+	}
+
+	var store = (0, _redux.createStore)(reducer, 0);
+
+	store.subscribe(function () {
+	    console.log(store.getState());
+	});
+
+	store.dispatch(action);
+	store.dispatch(action);
+	store.dispatch({ type: 'reduce' });
+
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
--- demo1/main.js
+++ demo1/main.js
@@ -2,4 +2,27 @@ import React    from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 
+var action = {
+    type : 'add'
+};
 
+function reducer(state, action){
+    switch(action.type){
+        case 'add':
+            return state+1;
+        case 'reduce':
+            return state-1;
+        default:
+            return state;
+    }
+}
+
+var store = createStore(reducer, 0);
+
+store.subscribe(function(){
+    console.log(store.getState());
+});
+
+store.dispatch(action);
+store.dispatch(action);
+store.dispatch({type : 'reduce'});
